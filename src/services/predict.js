87b0c9b71df5e5c649839f406ce43755d20bf57a
Electronica-dev <srsamartha@gmail.com>
2023-05-27 00:34:18 +0530

    remove response log

--- src/services/predict.js
+++ src/services/predict.js
@@ -3,7 +3,6 @@ const baseUrl = 'http://stock-predict-backend.onrender.com/api'
 
 const predict = async (symbol, interval, price, no_of_candles) => {
   const response = await axios.get(`${baseUrl}/stock/predict/${symbol}-${interval}-${price}-${no_of_candles}`)
-  console.log(response.data);
   return response.data
 }
 
